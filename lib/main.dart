ad2325dad95eea37a7947fa945cba4bc5ad025a6
chako-mountain <gonzaburouduanyewufu@gmail.com>
2025-12-01 22:43:37 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page '),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Position? _currentPosition; // 位置情報を格納する変数
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _determinePosition();
+  //   Position position = _determinePosition();
+  //   // debugPrint('My location: $mylocation');
+  //   _currentPosition = position;
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentPosition(); // 非同期関数を呼ぶ
+  }
+
+  void _getCurrentPosition() async {
+    final mapController = MapController();
+    try {
+      Position position = await _determinePosition();
+      setState(() {
+        _currentPosition = position; // State を更新
+      });
+
+      mapController.move(
+        LatLng(_currentPosition!.latitude, _currentPosition!.longitude),
+        15.0,
+      );
+    } catch (e) {
+      debugPrint('位置情報の取得に失敗: $e');
+    }
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  // final LocationSettings locationSettings = LocationSettings(
+  //   accuracy: LocationAccuracy.high,
+  //   distanceFilter: 100,
+  // );
+
+  // Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: .center,
+      //     children: [
+      //       const Text('You have pushed the button this many times:'),
+      //       Text(
+      //         '$_counter',
+      //         style: Theme.of(context).textTheme.headlineMedium,
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      body: FlutterMap(
+        options: MapOptions(
+          // 名古屋駅の緯度経度です。
+          // initialCenter: LatLng(35.170915, 136.881537),
+          initialCenter: _currentPosition != null
+              ? LatLng(_currentPosition!.latitude, _currentPosition!.longitude)
+              : LatLng(35.170915, 136.881537), // 位置情報がまだ取得できていない場合のデフォルト位置
+          initialZoom: 10.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // 位置情報サービスが有効かどうかをテストします。
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // 位置情報サービスが有効でない場合、続行できません。
+    // 位置情報にアクセスし、ユーザーに対して
+    // 位置情報サービスを有効にするようアプリに要請する。
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    // ユーザーに位置情報を許可してもらうよう促す
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // 拒否された場合エラーを返す
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  // 永久に拒否されている場合のエラーを返す
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.',
+    );
+  }
+
+  // ここまでたどり着くと、位置情報に対しての権限が許可されているということなので
+  // デバイスの位置情報を返す。
+  return await Geolocator.getCurrentPosition();
+}
